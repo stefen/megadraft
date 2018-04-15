ed5dd97c6e8ca89ad96c1f09b4ed6a84d30416cc
Marcelo Jorge Vieira <metal@alucinados.com>
2018-04-15 15:23:50 -0300

    Preetier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: "module",
+    indentSwitchCase: true,
+  },
+  globals: {
+    process: true,
+    ADMIN_ENV: true,
+  },
+  plugins: ["react", "jest", "prettier"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    "prettier/prettier": "error",
+  },
+  overrides: [
+    {
+      files: ["*.test.js"],
+      rules: { "react/prop-types": 0 },
+    },
+  ],
+}
